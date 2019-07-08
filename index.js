bd4729357acd871b13fd58a780b82b56da05bb5f
Shehzad Abdulcadir <shehzadac130@gmail.com>
2019-07-08 22:31:46 +0100

    [Form] - Add basic styles to form

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // Import SCSS
 import './scss/styles.scss'
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCcgk5uQ7BMLD99bom55tqD_MZLQgs254I",
+    authDomain: "shez-a5a8d.firebaseapp.com",
+    databaseURL: "https://shez-a5a8d.firebaseio.com",
+    projectId: "shez-a5a8d",
+    storageBucket: "shez-a5a8d.appspot.com",
+    messagingSenderId: "195337069678",
+    appId: "1:195337069678:web:946ec548706c7d7f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
