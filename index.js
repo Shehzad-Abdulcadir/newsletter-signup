1a3b02a1b758ad571769e8f8c8180c7723acb347
Shehzad Abdulcadir <shehzadac130@gmail.com>
2019-07-09 19:24:55 +0100

    [JS] - Add an email validation function

--- index.js
+++ index.js
@@ -24,11 +24,16 @@ document.getElementById('newsletter-form').addEventListener('submit', submitForm
 function submitForm(e){
     e.preventDefault();
 
-    //Get email value
-    const email = getInputVal('newsletter-email');
+    //Get email value and convert it to lowercase
+    const email = getInputVal('newsletter-email').toLowerCase();
 
-    // Save message
-    saveMessage(email);
+    if (validateEmail(email)) {
+        // Run saveEmail function
+        saveEmail(email);
+    } else {
+        // Throw an error if the email address entered is invalid
+        alert('Error! Please enter a valid email address.')
+    }
 }
 
 // Function to get form value (Email Address)
@@ -36,19 +41,25 @@ function getInputVal(id){
     return document.getElementById(id).value;
 }
 
+// Email Validation function
+function validateEmail(email) {
+    var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+    return re.test(email);
+}
+
 // Save email to firebase
-function saveMessage(email){
+function saveEmail(email){
 
     emailRef.once('value')
         // Return snapshot of the DB
         .then(function(dataSnapshot) {
             // Returns DB entries as collection of objects
-            const lol = Object.values(dataSnapshot.val());
+            const databaseObj = Object.values(dataSnapshot.val());
 
             // Filters to the first item in the object (in our case it's users) which returns another
             // set of objects that are our email address key/values (Email: Value).
             // Returns just the value from the object
-            const obj = Object.values(lol[0]);
+            const obj = Object.values(databaseObj[0]);
 
             // Init empty Array
             let arr = [];
@@ -58,27 +69,25 @@ function saveMessage(email){
                 arr = arr.concat(Object.values(val));
             });
 
-            // if email address is included in our array then throw some sort of error to say it exists.
+            // If email address is included in our array then throw some sort of error to say it exists.
             if (arr.includes(email)) {
-                console.log('it exists');
+                alert('Error! The Email address already exists in the database.')
 
-            //If it doesn't exist then post value to database
+            // If it doesn't exist then post value to database
             } else {
                 emailRef.child('users').push().set({
                     'email': email,
                 });
-                alert('Your Email address has been saved!');
+                alert('Success! Your Email address has been saved!');
             }
 
             // If the database is empty, it throws an error so within the error catch,
             // add the value to the database
         }).catch(err => {
-            console.log(err)
-
             emailRef.child('users').push().set({
                 'email': email,
             });
-            alert('Your Email address has been saved!');
+            alert('Success! Your Email address has been saved!');
     })
 
 }
